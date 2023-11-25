e6dba32047e1e9a098140c425c4ec78ee914ed33
Theophil Dio <dtheo.sangma@gmail.com>
2023-11-25 13:15:57 +0600

    Added simple request response practice to repo

---
+++ app/Http/Controllers/ProfileController.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Response;
+
+class ProfileController extends Controller
+{
+    public function index($id)
+    {
+        $name = "Donal Trump";
+        $age = "75";
+
+        $data = [
+            "id" => $id,
+            "name" => $name,
+            "age" => $age,
+        ];
+
+        $name = 'access_token';
+        $value = '123-XYZ';
+        $minutes = '1';
+        $path = '/';
+        $domain = $_SERVER['SERVER_NAME'];
+        $secure = false;
+        $httpOnly = true;
+
+        $response = new Response(json_encode($data), 200);
+        $response->cookie($name, $value, $minutes, $path, $domain, $secure, $httpOnly);
+
+        return $response;
+    }
+}
